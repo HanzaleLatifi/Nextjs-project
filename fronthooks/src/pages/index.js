c6c349766b77032375d01698d24f5d0d02dbd13b
h8 <h.latifi03@gmail.com>
2022-09-03 21:55:18 +0430

    desktop sort secion

--- fronthooks/src/pages/index.js
+++ fronthooks/src/pages/index.js
@@ -1,4 +1,4 @@
-import { ChevronDownIcon } from '@heroicons/react/24/outline';
+import { ChevronDownIcon ,AdjustmentsHorizontalIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import { useState } from 'react';
 
@@ -6,11 +6,12 @@ export default function Home() {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <div className="grid gap-4 md:grid-cols-12 md:grid-rows-2 bg-gray-100 min-h-screen">
+    <div className="grid gap-4 md:grid-cols-12 bg-gray-100 md:grid-rows-[60px_minmax(300px,_1fr)] min-h-screen ">
+      {/* desktop category */}
       <div className="hidden  md:block md:col-span-3 md:row-span-2 bg-emerald-50">
           <div className='bg-white rounded-xl overflow-hidden'>
             {/* accordian header */}
-            <div className='flex items-center justify-between bg-green-400 py-4 px-4  cursor-pointer' onClick={()=>setIsOpen(!isOpen)} >
+            <div className='flex items-center justify-between bg-green-400 py-3 px-4  cursor-pointer' onClick={()=>setIsOpen(!isOpen)} >
                 <span className='text-lg font-medium' >دسته بندی مقالات</span>
                 <ChevronDownIcon className={`h-6 transition-all  w-6 ${isOpen ? 'rotate-180':'rotate-0'}`}/>
             </div>
@@ -34,9 +35,21 @@ export default function Home() {
             </div>
           </div>
       </div>
-      <div className="hidden md:block md:col-span-9 bg-emerald-50">
-        sort
+      {/* desktop sort */}
+      <div className="hidden md:block md:col-span-9 ">
+        <div className='bg-white rounded-xl px-4 flex'>
+          <div className='flex items-center gap-x-2 ml-6 '>
+            <AdjustmentsHorizontalIcon className='h-6 w-6 '/>
+            <p>مرتب سازی :</p>
+          </div>
+          <ul className='flex gap-x-6 '>
+            <li className='py-4 cursor-pointer'>جدید ترین</li>
+            <li className='py-4 cursor-pointer'>محبوب ترن</li>
+            <li className='py-4 cursor-pointer'>پربازدید ترین</li>
+          </ul>
+        </div>     
       </div>
+      {/* blogs */}
       <div className="md:col-span-9 bg-emerald-50">
         blogs
       </div>
